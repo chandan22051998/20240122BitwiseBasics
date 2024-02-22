2f2d53abf1104259341eef91b02e3533b9d03bcc
chandan22051998 <160845768+chandan22051998@users.noreply.github.com>
2024-02-22 15:34:20 +0530

    Add files via upload

---
+++ BitwiseBasics.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdint.h>
+
+typedef uint8_t datatype;
+
+void printBinary(datatype  value);
+
+void main(int argc, char *argv[])
+{
+    datatype value = 100, position = 0;
+
+    /* Validate command line arguments s->set, g->get, t->toggle r->reset */
+    if(argc != 2)
+    {
+        printf("Usage error \n ./exe s/t/g");
+    }
+
+    printBinary(value);
+
+    if((*argv[1] == 's') || (*argv[1] == 'S'))
+    {
+        printf("Enter the bit number to be set\n");
+        scanf("%d", &position);
+        value |= (1 << position);
+        printf("After bit is set\n");
+        printBinary(value);
+    }   
+}
+
+/* Print the Binary of the value*/
+void printBinary(datatype  value)
+{
+    for(int8_t iterate = 7; iterate >= 0; iterate--)
+    {
+        printf("%d ", ((value >> iterate) & 1));
+    }
+    printf("\n");
+}
