eff76156d6f673003e863b168b59dd8d6ef77e21
chandan22051998 <160845768+chandan22051998@users.noreply.github.com>
2024-02-27 17:19:13 +0530

    Add files via upload

--- BitwiseBasics.c
+++ BitwiseBasics.c
@@ -1,38 +1,76 @@
 #include <stdio.h>
 #include <stdint.h>
 
-typedef uint8_t datatype;
+/* If you change the datatype ensure to change the format specifer in scanf function */
+typedef uint16_t datatype; 
 
 void printBinary(datatype  value);
+void set(datatype  value);
+void toggle(datatype  value);
+void reset(datatype  value);
 
 void main(int argc, char *argv[])
 {
     datatype value = 100, position = 0;
 
     /* Validate command line arguments s->set, g->get, t->toggle r->reset */
-    if(argc != 2)
+    if (argc != 2)
     {
         printf("Usage error \n ./exe s/t/g");
     }
     printBinary(value);
-
-    if((*argv[1] == 's') || (*argv[1] == 'S'))
+    switch(*argv[1])
     {
-        printf("Enter the bit number to be set\n");
-        scanf("%hhd", &position);
-        value = value | (1 << position);
-        printf("After bit is set\n");
-        printBinary(value);
-    }   
+        case 's':
+        case 'S': set(value);
+                  break;
+        case 't':
+        case 'T': toggle(value);
+                  break;
+        case 'r':
+        case 'R': reset(value);
+                break;
+        default : break;
+    } 
 }
 
 /* Print the Binary of the value*/
 void printBinary(datatype  value)
 {
-    for(int8_t iterate = 7; iterate >= 0; iterate--)
+    for(int8_t iterate = ( (sizeof(datatype) * 8) -1); iterate >= 0; iterate--)
     {
-        printf("%d ", ((value >> iterate) & 1));
+        printf("%hd ", ((value >> iterate) & 1));
     }
     printf("\n");
-    printf("Inside for Valus is : %d\n", value);
+    printf("Inside for Valus is : %hd\n", value);
+}
+/* Function to set a bit */
+void set(datatype  value)
+{
+    datatype position = 0;
+    printf("Enter the bit number to be set\n");
+    scanf("%hd", &position);
+    value = value | (1 << position);
+    printf("After bit is set\n");
+    printBinary(value);
+}
+/* Function to toggle a bit */
+void toggle(datatype  value)
+{
+    datatype position = 0;
+    printf("Enter the bit number to be reset\n");
+    scanf("%hd", &position);
+    value = value ^ (1 << position);
+    printf("After bit is toggle\n");
+    printBinary(value);
+}
+/* Function to reset a bit */
+void reset(datatype  value)
+{
+    datatype position = 0;
+    printf("Enter the bit number to be reset\n");
+    scanf("%hd", &position);
+    value = value & (~(1 << position));
+    printf("After bit is reset\n");
+    printBinary(value);
 }
